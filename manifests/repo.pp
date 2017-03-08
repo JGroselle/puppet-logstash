7e5885662c3604adbf519edeeaf7fda4790ab86a
jgroselle <julien.groselle@sicpa.com>
2017-03-08 08:42:25 +0100

    use http repo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -11,7 +11,7 @@
 class logstash::repo {
   $version = $logstash::repo_version
   $repo_name = "elastic-${version}"
-  $url_root = "https://artifacts.elastic.co/packages/${version}"
+  $url_root = "http://artifacts.elastic.co/packages/${version}"
   $gpg_key_url = 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
   $gpg_key_id = '46095ACC8548582C1A2699A9D27D666CD88E42B4'
 
